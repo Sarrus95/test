348d3aad7062bce361812112c1f7a32f0df75579
TigerTigre88 <luposolitario1712@gmail.com>
2025-04-01 12:23:34 +0200

    funzione div

--- src/test.ts
+++ src/test.ts
@@ -1,9 +1,10 @@
 const sum = (a: number, b: number) => a + b;
 const molt = (a: number, b: number) => a * b;
-const div = (a: number, b:) => a / b;
+const div = (a: number, b:number) => a / b;
 
 const num1 = 12;
 const num2 = 23;
 
 console.log(sum(num1, num2));
 console.log(molt(num1, num2));
+console.log(div(num1, num2));
