ed49ae3ed1a6b97d6557f97c974a0e371935ffaf
Sarrus95 <rosariozago@outlook.it>
2025-04-01 12:08:23 +0200

    feat: :sparkles: Difference function
    Added difference function

--- src/test.ts
+++ src/test.ts
@@ -1,6 +1,8 @@
-const sum = (a: number,b: number) => a + b;
-
 const num1 = 12;
 const num2 = 23;
 
+const sum = (a: number,b: number) => a + b;
+const diff = (a: number,b: number) => a - b;
+
 console.log(sum(num1,num2));
+console.log(diff(num1,num2));
