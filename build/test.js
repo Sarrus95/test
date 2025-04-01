956aa3eaa640c4547acb9f99869258fd29abd556
Sarrus95 <rosariozago@outlook.it>
2025-04-01 11:57:38 +0200

    initial commit

---
+++ build/test.js
@@ -0,0 +1,5 @@
+"use strict";
+const sum = (a, b) => a + b;
+const num1 = 12;
+const num2 = 23;
+console.log(sum(num1, num2));
---
+++ src/test.ts
@@ -0,0 +1,6 @@
+const sum = (a: number,b: number) => a + b;
+
+const num1 = 12;
+const num2 = 23;
+
+console.log(sum(num1,num2));
